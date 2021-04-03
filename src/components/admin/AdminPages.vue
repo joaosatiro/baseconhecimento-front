40c7ccf9b68a31957ef685b3ff583effd5e89410
João Sátiro <joao.v.satiro@gmail.com>
2021-04-03 19:45:30 -0300

    config router admin and home

---
+++ src/components/admin/AdminPages.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="admin-pages">
+        <Page-title icon="fa fa-cogs" main="Administração do Sistema"
+        sub="Cadastro & Cia" />
+    </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle';
+
+export default {
+    name:'AdminPages',
+    components: { PageTitle }
+};
+</script>
+
+<style>
+
+</style>
