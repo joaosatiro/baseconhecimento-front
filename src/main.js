53f83543a495d3c7b4984b43d04c07a4f5dd7479
João Sátiro <joao.v.satiro@gmail.com>
2021-04-04 15:23:56 -0300

    create component stat

--- src/main.js
+++ src/main.js
@@ -9,6 +9,9 @@ import router from './config/router';
 
 Vue.config.productionTip = false;
 
+// eslint-disable-next-line no-undef
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkpvYW8gU2F0aXJvIiwiZW1haWwiOiJqb2Fvc2F0aXJvMTdAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYxNzU1OTgwNiwiZXhwIjoxNjE3ODE5MDA2fQ.umslQ101_lQ0lKvtovEjW3Z2D2BlhLf0quC-HMr-TxM';
+
 new Vue({
     store,
     router,
